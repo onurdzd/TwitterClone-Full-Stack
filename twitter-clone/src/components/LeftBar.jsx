3251b398502b4726099e7c6916227781203b605c
onurd <106928484+onurdzd@users.noreply.github.com>
2023-08-25 14:58:16 +0300

    updates

--- twitter-clone/src/components/LeftBar.jsx
+++ twitter-clone/src/components/LeftBar.jsx
@@ -61,7 +61,7 @@ const LeftBar = () => {
       </section>
 
       <section className="hidden lg:w-[30%] lg:flex flex-col items-center gap-2 h-screen text-xl justify-between fixed">
-        <nav className="flex flex-col gap-5 h-[70%] cursor-pointer ">
+        <nav className="flex flex-col gap-2 h-[70%] cursor-pointer ">
           <div className="max-w-fit hover:bg-slate-100 rounded-full pt-3 p-2 scale-125">
             <svg width="25" height="25">
               <path d="M18.244 2.25h3.308l-7.227 8.26 8.502 11.24H16.17l-5.214-6.817L4.99 21.75H1.68l7.73-8.835L1.254 2.25H8.08l4.713 6.231zm-1.161 17.52h1.833L7.084 4.126H5.117z"></path>
