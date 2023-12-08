9241a07cfb10cf67a599e0d9948b794235d939c9
onurd <106928484+onurdzd@users.noreply.github.com>
2023-12-08 14:19:17 +0300

    profile info added

--- twitter-clone/src/components/Profile.jsx
+++ twitter-clone/src/components/Profile.jsx
@@ -70,6 +70,27 @@ export const Profile = () => {
             </header>
             <main className="flex justify-end ">
               <div className="max-[600px]:w-full lg:w-[45%] max-[1025px]:w-[85%]">
+                <div className="px-4 py-4">
+                  <div className="font-bold flex text-base items-center">
+                    {user.name}
+                    <span className="scale-90">
+                      <svg width="20" height="25">
+                        <path d="M17.5 7H17v-.25c0-2.76-2.24-5-5-5s-5 2.24-5 5V7h-.5C5.12 7 4 8.12 4 9.5v9C4 19.88 5.12 21 6.5 21h11c1.39 0 2.5-1.12 2.5-2.5v-9C20 8.12 18.89 7 17.5 7zM13 14.73V17h-2v-2.27c-.59-.34-1-.99-1-1.73 0-1.1.9-2 2-2 1.11 0 2 .9 2 2 0 .74-.4 1.39-1 1.73zM15 7H9v-.25c0-1.66 1.35-3 3-3 1.66 0 3 1.34 3 3V7z"></path>
+                      </svg>
+                    </span>
+                  </div>
+                  <div className="font-light text-sm">@{user.username}</div>
+                  <div className="flex items-center py-2 ">
+                    <svg width="25" height="25">
+                      <path d="M7 4V3h2v1h6V3h2v1h1.5C19.89 4 21 5.12 21 6.5v12c0 1.38-1.11 2.5-2.5 2.5h-13C4.12 21 3 19.88 3 18.5v-12C3 5.12 4.12 4 5.5 4H7zm0 2H5.5c-.27 0-.5.22-.5.5v12c0 .28.23.5.5.5h13c.28 0 .5-.22.5-.5v-12c0-.28-.22-.5-.5-.5H17v1h-2V6H9v1H7V6zm0 6h2v-2H7v2zm0 4h2v-2H7v2zm4-4h2v-2h-2v2zm0 4h2v-2h-2v2zm4-4h2v-2h-2v2z"></path>
+                    </svg>
+                    <span className="font-light">Temmuz 2015'de katıldı</span>
+                  </div>
+                    <div className="flex">
+                      <div className="font-light"><span className="font-bold">500</span> Takip Edilen</div>
+                      <div className="ml-5 font-light"><span className="font-bold">450</span> Takipçi</div>
+                    </div>
+                </div>
                 <div className="flex">
                   <div
                     onClick={() => setBordered(0)}
